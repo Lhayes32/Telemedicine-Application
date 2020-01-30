e17a831948af6f83f6f1f587778bba61477038ff
Rohan Desai <rrdesai93@gmail.com>
2020-01-30 12:23:54 -0500

    Moved folder to the correct location

--- src/app/myfiles/myfiles.component.ts
+++ src/app/myfiles/myfiles.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 export interface FileList {
   name: string;
   date: string;
@@ -11,6 +15,7 @@ const FILE_DATA: FileList[] = [
   {name: 'FileUpload04.jpg', date: "01-04-2020"},
   {name: 'FileUpload05.jpg', date: "01-05-2020"},
 ];
+
 @Component({
   selector: 'app-myfiles',
   templateUrl: './myfiles.component.html',
@@ -19,9 +24,40 @@ const FILE_DATA: FileList[] = [
 export class MyfilesComponent implements OnInit {
   displayedColumns: string[] = ['name', 'date'];
   dataSource = FILE_DATA;
-  constructor() { }
+  displayemail: string;
+
+  constructor(
+    private authService: AuthService,
+    public afAuth: AngularFireAuth,
+    public afs: AngularFirestore,   // Inject Firestore service
+  ) {
+  }
 
   ngOnInit() {
+
+    try {
+      this.displayemail = this.afAuth.auth.currentUser.email;
+      localStorage.setItem("displayemail", this.displayemail);
+      console.log(this.displayemail);
+    } catch (error) {
+      this.displayemail = localStorage.getItem("displayemail");
+      console.log(this.displayemail);
+    }
+
+  }
+
+  isMenuOpen = true;
+  contentMargin = 240;
+
+  onToolbarMenuToggle() {
+    console.log('On toolbar toggled', this.isMenuOpen);
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (!this.isMenuOpen) {
+      this.contentMargin = 70;
+    } else {
+      this.contentMargin = 240;
+    }
   }
 
 }
