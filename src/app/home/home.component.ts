22c5847bf5f014c4d0fead747b1d5dc663cfcbb8
TylerOdom <todom741@gmail.com>
2020-02-20 18:13:21 -0500

    patients/doctors array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,6 +33,9 @@ export class HomeComponent implements OnInit {
   isDoctorDisplay:string;
   displayedColumns: string[] = ['doctor', 'date', 'appointment'];
   dataSource = ELEMENT_DATA;
+  firstandlastname = this.lastNameDisplay + " " + this.firstNameDisplay;
+  doctors:string[] = [];
+  patients:string[] = [];
 
   constructor(
     private authService: AuthService,
@@ -75,6 +78,39 @@ export class HomeComponent implements OnInit {
   }).catch(function(error) {
       console.log("Error getting document:", error);
   });
+  
+  // Take the names of users of opposing status and push to list.
+  this.afs.collection('users').get().toPromise()
+    .then(querySnapshot => {
+      querySnapshot.docs.forEach(doc => {
+        // If you are a doctor
+        if (this.isDoctorDisplay == "Doctor") {
+          // and they are not a doctor
+          if (doc.data().isDoctor == false) {
+            // and they are not you
+            if (doc.data().firstName != this.firstNameDisplay) {
+              // then print their name
+              console.log(doc.data().firstName + " " + doc.data().lastName);
+              this.patients.push(doc.data().firstName + " " + doc.data().lastName);
+              console.log(this.patients);
+              } 
+            }
+          }
+        // If you are a patient
+        if (this.isDoctorDisplay == "Patient") {
+          // and they are a doctor
+          if (doc.data().isDoctor == true) {
+            // and they are not you
+            if (doc.data().firstName != this.firstNameDisplay) {
+              // then print their name
+              console.log("Dr. " + doc.data().firstName + " " + doc.data().lastName);
+              this.doctors.push("Dr. " + doc.data().firstName + " " + doc.data().lastName);
+              console.log(this.doctors);
+            }
+            }
+          } 
+      });
+    });
 
     try {
       this.displayemail = this.afAuth.auth.currentUser.email;
@@ -99,38 +135,6 @@ export class HomeComponent implements OnInit {
     } else {
       this.contentMargin = 240;
     }
-  }
-
-  // This is the function to display the first names of users.
-  retrievedata() {
-    this.afs.collection('users').get().toPromise()
-    .then(querySnapshot => {
-      querySnapshot.docs.forEach(doc => {
-        // If you are a doctor
-        if (this.isDoctorDisplay == "Doctor") {
-          // and they are not a doctor
-          if (doc.data().isDoctor == false) {
-            // and they are not you
-            if (doc.data().firstName != this.firstNameDisplay) {
-              // then print their name
-              console.log(doc.data().firstName + " " + doc.data().lastName);
-              } 
-            }
-          }
-        // If you are a patient
-        if (this.isDoctorDisplay == "Patient") {
-          // and they are a doctor
-          if (doc.data().isDoctor == true) {
-            // and they are not you
-            if (doc.data().firstName != this.firstNameDisplay) {
-              // then print their name
-              console.log("Dr. " + doc.data().firstName + " " + doc.data().lastName);
-            }
-            }
-          } 
-          // If you are not a doctor
-      });
-    });
   }
 }
 
