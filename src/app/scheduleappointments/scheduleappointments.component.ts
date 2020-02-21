7237cdbc059e820f8dea7aa23116c8da6b351982
Tyler Odom <todom741@gmail.com>
2020-02-20 23:23:53 -0500

    Added dialog with patient/doctor

--- src/app/scheduleappointments/scheduleappointments.component.ts
+++ src/app/scheduleappointments/scheduleappointments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 export interface userdoc {
@@ -17,20 +19,81 @@ export interface time {
   styleUrls: ['./scheduleappointments.component.css']
 })
 export class ScheduleappointmentsComponent implements OnInit {
-
+  displayuid: string;
+  isDoctorDisplay:string;
+  doctors:string[] = [];
+  firstNameDisplay: string;
+  lastNameDisplay: string;
+  patients:string[] = [];
   userdoc: userdoc[] = [
-    {doctor: 'Ronak', value: 'Ronak-1' },
-    {doctor: 'Rohan', value: 'Rohan-2' },
-    {doctor: 'Tyler', value: 'Tyler-3'}
   ];
+  count = 1;
 
   time: time[] = [
     {value: '8 AM'}, {value: '9 AM'}, {value: '10 AM'}, {value: '11 AM'}, {value: '12 PM'}, {value: '1 PM'}, {value: '2 PM'}, {value: '3 PM'}, {value: '4 PM'}, {value: '5 PM'}, {value: '6 PM'},
   ];
 
-  constructor() { }
+  constructor(public afs: AngularFirestore,
+    public afAuth: AngularFireAuth,) { }
 
   ngOnInit() {
+
+    try {
+      this.displayuid = this.afAuth.auth.currentUser.uid
+      localStorage.setItem("displayuid", this.displayuid);
+    } catch (error) {
+      this.displayuid = localStorage.getItem("displayuid");
+    }
+
+    var docRef = this.afs.collection('users').doc(this.displayuid);
+
+    docRef.get().toPromise().then((doc) => {
+      if (doc.exists) {
+          this.firstNameDisplay = doc.data().firstName;
+          this.lastNameDisplay = doc.data().lastName;
+          if (doc.data().isDoctor) {
+            this.isDoctorDisplay = "Doctor";
+          } else {
+            this.isDoctorDisplay = "Patient";
+          }
+      } else {
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+
+    this.afs.collection('users').get().toPromise()
+    .then(querySnapshot => {
+      querySnapshot.docs.forEach(doc => {
+        // If you are a doctor
+        if (this.isDoctorDisplay == "Doctor") {
+          // and they are not a doctor
+          if (doc.data().isDoctor == false) {
+            // and they are not you
+            if (doc.data().firstName != this.firstNameDisplay) {
+              // then print their name
+              var test = {doctor: doc.data().firstName + " " + doc.data().lastName, value: this.count.toString()}
+              this.userdoc.push(test);
+              this.count = this.count + 1;
+              } 
+            }
+          }
+        // If you are a patient
+        if (this.isDoctorDisplay == "Patient") {
+          // and they are a doctor
+          if (doc.data().isDoctor == true) {
+            // and they are not you
+            if (doc.data().firstName != this.firstNameDisplay) {
+              // then print their name
+              var test = {doctor: "Dr. " + doc.data().firstName + " " + doc.data().lastName, value: this.count.toString()}
+              this.userdoc.push(test);
+              this.count = this.count + 1;
+            }
+            }
+          } 
+      });
+    });
   }
 
 }
