40defac514f9c72570406fbf331e694a4b32e7d6
Leonard Hayes <lhayes32@students.kennesaw.edu>
2020-04-13 19:58:29 -0400

    Updated ui a bit. Trying to get it to work similarly to how chat works.

--- src/app/videocall/videocall.component.ts
+++ src/app/videocall/videocall.component.ts
@@ -2,7 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { NgxAgoraService, Stream, AgoraClient, ClientEvent, StreamEvent } from 'ngx-agora';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from '../auth.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {FormControl} from '@angular/forms';
+
+
+export interface userdoc {
+  doctor: string;
+  email: string;
+  uid: string;
+}
+
+interface userGroup {
+  _category: string;
+  _allUsers: any;
+}
 
 @Component({
   selector: 'app-videocall',
@@ -10,9 +26,41 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./videocall.component.css']
 })
 export class VideocallComponent implements OnInit {
-
+  displayuid: string;
+  displayemail: string;
+  firstNameDisplay: string;
+  lastNameDisplay: string;
+  isDoctorDisplay: string;
+  surname: string;
+  isDoctor: boolean;
+  doctordoc:userdoc[] = [];
+  patientdoc:userdoc[] = [];
+  appointmentdoc:userdoc[] = [];
+  checkbool: boolean;
+  flag: boolean;
+  selectedappointment: string;
+  expenses: any;
+  books: any;
+  value: any;
+  selecteduid: string;
   localCallId = 'agora_local';
   remoteCalls: string[] = [];
+
+  userControl = new FormControl();
+  userGroups: userGroup[] = [
+    {
+      _category: 'Appointments With',
+      _allUsers: this.appointmentdoc,
+    },
+    {
+      _category: 'Doctor',
+      _allUsers: this.doctordoc,
+    },
+    {
+      _category: 'Patient',
+      _allUsers: this.patientdoc,
+    },
+  ];
 
   private client: AgoraClient;
   private localStream: Stream;
@@ -34,8 +82,22 @@ export class VideocallComponent implements OnInit {
       console.log("UID (Catch):   " + this.uid)
     }
   }
+  
 
   ngOnInit() {
+    try {
+      this.displayuid = this.afAuth.auth.currentUser.uid
+      localStorage.setItem("displayuid", this.displayuid);
+    } catch (error) {
+      this.displayuid = localStorage.getItem("displayuid");
+    }
+    try {
+      this.displayemail = this.afAuth.auth.currentUser.email;
+      localStorage.setItem("displayemail", this.displayemail);
+    } catch (error) {
+      this.displayemail = localStorage.getItem("displayemail");
+    }
+    
     this.client = this.ngxAgoraService.createClient({ mode: 'rtc', codec: 'h264' });
     this.assignClientHandlers();
 
@@ -43,6 +105,110 @@ export class VideocallComponent implements OnInit {
     this.localStream = this.ngxAgoraService.createStream({ streamID: this.uid, audio: true, video: true, screen: false });
     this.assignLocalStreamHandlers();
     this.initLocalStream(() => this.join(uid => this.publish(), error => console.error(error)));
+    // Fetch user's data
+    this.fetchuserdata()
+
+    // Update doctordoc
+    this.updateDoctorsPatients()
+    
+    // Update appointments
+    this.updateappointments()
+    
+  }
+
+  fetchuserdata() {
+    // Retrieve user data
+    var docRef = this.afs.collection('users').doc(this.displayuid);
+    docRef.get().toPromise().then((doc) => {
+      if (doc.exists) {
+          this.firstNameDisplay = doc.data().firstName;
+          this.lastNameDisplay = doc.data().lastName;
+          if (doc.data().isDoctor == true) {
+            this.isDoctorDisplay = "Doctor";
+            this.surname = "Dr. "
+            this.isDoctor = true;
+          } else {
+            this.isDoctorDisplay = "Patient";
+            this.isDoctor = false;
+          }
+      } else {
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+  }
+
+  // This method updates the selection list on the chat page with doctors and patients.
+  updateDoctorsPatients() {
+    this.afs.collection('users').get().toPromise()
+    .then(querySnapshot => {
+      querySnapshot.docs.forEach(doc => {
+          // If they are a doctor
+          if (doc.data().isDoctor == true) {
+            // and they are not you
+            if (doc.data().uid != this.displayuid) {
+              // Remove all users without names
+              if (!(doc.data().firstName == null || doc.data().firstName == null || doc.data().firstName == "" || doc.data().lastName == "")) {
+                // then print their name
+                var test = {doctor: doc.data().firstName + " " + doc.data().lastName, email: doc.data().email, uid: doc.data().uid}
+                this.doctordoc.push(test);
+              }
+            }
+          }
+          if (doc.data().isDoctor == false) {
+            // and they are not you
+            if (doc.data().uid != this.displayuid) {
+              // Remove all users without names
+              if (!(doc.data().firstName == null || doc.data().firstName == null || doc.data().firstName == "" || doc.data().lastName == "")) {
+                // then print their name
+                var test = {doctor: doc.data().firstName + " " + doc.data().lastName, email: doc.data().email, uid: doc.data().uid}
+                this.patientdoc.push(test);
+                }
+              }
+            }
+        }
+        );
+        });
+    }
+
+    // This method updates the selection list on the chat page with people who you have appointments.
+    updateappointments() {
+      this.afs.collection('appointments').get().toPromise()
+      .then(querySnapshot => {
+        querySnapshot.docs.forEach(doc => {
+          this.checkbool = true;
+          // If the current doc has the user's uid in it as the sender or receiver.
+          if (this.displayuid == doc.data().senderuid)
+          {
+            for (var i = 0; i < this.appointmentdoc.length; i++) {
+              if (this.appointmentdoc[i].uid == doc.data().receiveruid)
+                {
+                  this.checkbool = false;
+                }
+              }
+            if (this.checkbool == true)
+              {
+                var test = {doctor: doc.data().receiver, email: doc.data().receiveremail, uid: doc.data().receiveruid}
+                this.appointmentdoc.push(test);
+              }
+              } 
+          if (this.displayuid == doc.data().receiveruid)
+          {
+            for (var i = 0; i < this.appointmentdoc.length; i++) {
+              if (this.appointmentdoc[i].uid == doc.data().senderuid)
+                {
+                  this.checkbool = false;
+                }
+              }
+            if (this.checkbool == true)
+              {
+                var test = {doctor: doc.data().sender, email: doc.data().senderemail, uid: doc.data().senderuid}
+                this.appointmentdoc.push(test);
+              }
+              } 
+      });
+    })
   }
 
   /**
